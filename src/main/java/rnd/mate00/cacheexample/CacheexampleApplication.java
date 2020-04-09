1c4bf20c459fb5b47d07ea28dfd67a4e5fbec3de
mate00 <mami.ext@gmail.com>
2020-04-09 07:05:38 +0200

    First commit

---
+++ src/main/java/rnd/mate00/cacheexample/CacheexampleApplication.java
@@ -0,0 +1,13 @@
+package rnd.mate00.cacheexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheexampleApplication.class, args);
+	}
+
+}
